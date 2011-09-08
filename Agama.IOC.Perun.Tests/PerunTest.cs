0a3e1e444fe6ad33f4ab5ab0f523535367236bbd
Vladka Janu <vladkax@volny.cz>
2011-09-09 00:20:36 +0200

    base2

---
+++ Agama.IOC.Perun.Tests/PerunTest.cs
@@ -0,0 +1,123 @@
+﻿using System.Threading;
+using Agama.IOC.Tests;
+using Agama.Services.Api.IOC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agama.IOC.Perun.Tests
+{
+    [TestClass]
+    public class PerunTest : IocAdapterBaseTest
+    {
+        protected override IIocContainer CreateContainer()
+        {
+            return new PerunAdapter();
+        }
+
+       
+
+
+        [TestMethod]
+        public override void HttpContextScope()
+        {
+            base.HttpContextScope();
+        }
+
+        [TestMethod]
+        public override void ThreadScope()
+        {
+            base.ThreadScope();
+        }
+
+        [TestMethod]
+        public override void TransientScope()
+        {
+            base.TransientScope();
+        }
+        [TestMethod]
+        public override void SingletonScope()
+        {
+            base.SingletonScope();
+        }
+        [TestMethod]
+        public override void RegisterInstance()
+        {
+            base.RegisterInstance();
+        }
+
+        [TestMethod]
+        public override void IsConfiguredFor()
+        {
+            base.IsConfiguredFor();
+        }
+
+        [TestMethod]
+        public override void GetServices()
+        {
+            base.GetServices();
+        }
+
+        [TestMethod]
+        public override void Dispose1()
+        {
+            base.Dispose1();
+        }
+
+        [TestMethod]
+        public override void WiringEnumerable()
+        {
+            base.WiringEnumerable();
+        }
+        [TestMethod]
+        public override void WiringFunc()
+        {
+            base.WiringFunc();
+        }
+        [TestMethod]
+        public override void WiringLazy()
+        {
+            base.WiringLazy();
+        }
+
+        [TestMethod]
+        public override void Advanced1()
+        {
+            base.Advanced1();
+        }
+
+        [TestMethod]
+        public override void Advanced1b()
+        {
+            base.Advanced1b();
+        }
+
+        [TestMethod]
+        public override void Advanced2()
+        {
+            base.Advanced2();
+        }
+        
+        [TestMethod]
+        public override void Advanced3GetServices()
+        {
+            base.Advanced3GetServices();
+        }
+     
+
+        [TestMethod]
+        public override void Performace1()
+        {
+            base.Performace1();
+        }
+        [TestMethod]
+        public override void Performace2()
+        {
+            base.Performace2();
+        }
+
+        [TestMethod]
+        public override void Performace3()
+        {
+            base.Performace3();
+        }
+    }
+}
---
+++ Agama.IOC.Perun/PerunAdapter.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Agama.Perun;
+using Agama.Services.Api.IOC;
+
+namespace Agama.IOC.Perun
+{
+
+   
+
+    public class PerunAdapter : IIocContainer 
+    {
+        
+        
+        public PerunAdapter()
+        {
+            _ioc = new PerunContainer();
+        }
+
+        private readonly PerunContainer _ioc;
+
+        private IPerunScope GetScope(LifeTimeType lifeTimeType)
+        {
+            switch (lifeTimeType)
+            {
+                case LifeTimeType.Singleton:
+                    return _ioc;
+                case LifeTimeType.Thread:
+                    return ThreadScope.Instance;
+                case LifeTimeType.Transient:
+                    return TransientScope.Instance;
+                case LifeTimeType.HttpContext:
+                    return HttpContextScope.Instance; //todo:
+                default: throw new NotImplementedException();
+            }
+        }
+       
+
+        public bool IsConfiguredFor(Type interfaceType)
+        {
+            return _ioc.IsConfiguredFor(interfaceType);
+        }
+
+        public bool IsConfiguredFor<T>()
+        {
+            return _ioc.IsConfiguredFor<T>();
+        }
+
+        public T GetService<T>()
+        {
+            return _ioc.GetService<T>();
+        }
+
+        public object GetService(Type t)
+        {
+            return _ioc.GetService(t);
+        }
+
+        public IEnumerable<T> GetServices<T>()
+        {
+            return _ioc.GetServices<T>();
+           
+        }
+
+        public IEnumerable<object> GetServices(Type t)
+        {
+            return _ioc.GetServices(t);
+        }
+
+      
+
+        public void RegisterType<TReal>(LifeTimeType lifeTimeType)
+        {
+            _ioc.RegisterType<TReal>(GetScope(lifeTimeType));
+        }
+
+        public void RegisterType<TInterface, TReal>(LifeTimeType lifeTimeType) where TReal : TInterface
+        {
+            _ioc.RegisterType<TInterface,TReal>(GetScope(lifeTimeType));
+        }
+
+       public void RegisterType<TInterface>(Func<TInterface> builder, LifeTimeType lifeTimeType)
+       {
+           _ioc.RegisterType<TInterface>(builder,GetScope(lifeTimeType));
+       }
+
+       
+
+        public void RegisterType(Type type, Type @interface, LifeTimeType lifeTimeType)
+        {
+          _ioc.RegisterType(type,@interface, GetScope(lifeTimeType));
+        }
+
+       
+
+        
+        public void RegisterType(Type interfaceType, Func<object> builder, LifeTimeType lifeTimeType)
+        {
+           _ioc.RegisterType(interfaceType, builder,GetScope(lifeTimeType));
+        }
+
+      
+       
+
+        public void EjectServices(LifeTimeType lifeTimeType)
+        {
+          _ioc.EjectServices( GetScope(lifeTimeType));
+        }
+
+      
+
+        public bool? IsCleanupRequiredForScope(LifeTimeType lifeTimeType)
+        {
+            return null;
+        }
+
+
+           #region Dispose Block
+        /// <summary>
+        /// Returns <c>true</c>, if object is disposed.
+        /// </summary>
+        public bool Disposed { get; private set; }
+        /// <summary>
+        /// Implemetation of <see cref="IDisposable.Dispose"/>.
+        /// It calls Dispose on every scope-holded instance (if is <see cref="IDisposable"/>).
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (Disposed)
+                return;
+
+            Disposed = true;
+            _ioc.Dispose();
+            
+        }
+
+        ~PerunAdapter()
+        {
+            Dispose(false);
+        }
+        #endregion
+
+    }
+}
